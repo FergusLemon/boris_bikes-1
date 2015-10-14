a6ea13b513d7a1555947424ba4629ed1b8bf2f13
fergus lemon <fergus.lemon@gmail.com>
2015-10-14 11:27:38 +0100

    updates lib and spec files for #working? method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,4 +3,13 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike)}
 
+describe '#release_bike' do
+  it 'returns a new bike when #release_bike is called' do
+    expect(subject.release_bike).to be_instance_of Bike
+  end
+  it 'expects bike to be #working' do
+    expect(subject.release_bike).to be_working
+  end
+end
+
 end
