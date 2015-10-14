266d6ec48542483fa286f52a2593a6a4f6ef7b20
samover <samuel.overloop@gmail.com>
2015-10-14 16:49:46 +0100

    added emtpy? and full? private methods to #docking_station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,12 +8,22 @@ class DockingStation
   end
 
   def release_bike
-    raise 'This docking station is empty' if @bikes.empty?
+    raise 'This docking station is empty' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise 'This docking station is full' if @bikes.length >= 20
+    raise 'This docking station is full' if full?
     @bikes << bike
   end
+
+  private
+  def full?
+    @bikes.length >= 20 ? true : false
+  end
+
+  def empty?
+    @bikes.empty? ? true : false
+  end
+
 end
