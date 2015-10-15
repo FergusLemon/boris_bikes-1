46652459c6011b9b2bcd3d9df5f7bfb78ffa0a67
George Maddocks <gwpmad@Georges-MacBook-Pro.local>
2015-10-15 17:43:56 +0100

    Completed Challenge 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,7 @@
 require 'docking_station'
 
 describe DockingStation do
-  # let(:bike) {Bike.new}
+  let(:bike) {double :bike} #this creates a SINGLE double variable, that is invoked when we type 'bike' - it has the same object ID each time. (We think)
 
   it 'has a default capacity' do
     expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
@@ -10,26 +10,26 @@ describe DockingStation do
   it 'has a variable capacity' do
     variable_capacity = 23
     station = DockingStation.new(variable_capacity)
-    variable_capacity.times { station.dock(double(:bike))}
-    expect {station.dock(double(:bike))}.to raise_error "This docking station is full"
+    variable_capacity.times { station.dock(bike)}
+    expect {station.dock(bike)}.to raise_error "This docking station is full"
   end
 
   it { is_expected.to respond_to(:release_bike)}
-
+# we are using the following instructions to create the doubles with their method stubs: https://github.com/rspec/rspec-mocks#method-stubs
   describe '#release_bike' do
     it 'releases a bike when #release_bike is called, so long as the bike is working' do
-      subject.dock(double(:bike))
-      expect(subject.release_bike).to eq double(:bike)
+      working_bike = double(:working_status => true)
+      subject.dock (working_bike)
+      expect(subject.release_bike).to eq (working_bike)
     end
 
     it 'raises an error when user tries to release a broken bike' do
-      broken_bike = double(:bike)
-      broken_bike.report_broken
-      subject.dock(broken_bike)
+      subject.dock(double(:working_status => false))
       expect {subject.release_bike}.to raise_error "This bike is broken"
     end
+
     it 'expects bike to be working' do
-      subject.dock(double(:bike))
+      subject.dock (double(:working_status => true))
       expect(subject.release_bike.working_status).to eq true
     end
   end
@@ -39,8 +39,8 @@ describe DockingStation do
   it { is_expected.to respond_to(:bikes)}
 
   it 'shows me that my bike is docked' do
-    subject.dock(double(:bike))
-    expect(subject.bikes).to include(double(:bike))
+    subject.dock(bike)
+    expect(subject.bikes).to include(bike)
   end
 
   it 'raises an error when #release_bike and the station is empty' do
@@ -48,7 +48,7 @@ describe DockingStation do
   end
 
   it 'raises an error when docking a bike when docking station is full' do
-    DockingStation::DEFAULT_CAPACITY.times{subject.dock(double(:bike))}
-    expect {subject.dock(double(:bike))}.to raise_error("This docking station is full")
+    DockingStation::DEFAULT_CAPACITY.times{subject.dock(bike)}
+    expect {subject.dock(bike)}.to raise_error("This docking station is full")
   end
 end
