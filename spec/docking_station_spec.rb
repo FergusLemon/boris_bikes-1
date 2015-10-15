82102987d9fe1a928ff97b3953d9f7dbe66c59c9
fergus lemon <fergus.lemon@gmail.com>
2015-10-15 15:51:32 +0100

    updated DS spec file with doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,7 @@
 require 'docking_station'
 
 describe DockingStation do
-  let(:bike) {Bike.new}
+  # let(:bike) {Bike.new}
 
   it 'has a default capacity' do
     expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
@@ -10,26 +10,26 @@ describe DockingStation do
   it 'has a variable capacity' do
     variable_capacity = 23
     station = DockingStation.new(variable_capacity)
-    variable_capacity.times { station.dock(bike)}
-    expect {station.dock(bike)}.to raise_error "This docking station is full"
+    variable_capacity.times { station.dock(double(:bike))}
+    expect {station.dock(double(:bike))}.to raise_error "This docking station is full"
   end
 
   it { is_expected.to respond_to(:release_bike)}
 
   describe '#release_bike' do
     it 'releases a bike when #release_bike is called, so long as the bike is working' do
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
+      subject.dock(double(:bike))
+      expect(subject.release_bike).to eq double(:bike)
     end
 
     it 'raises an error when user tries to release a broken bike' do
-      broken_bike = bike
+      broken_bike = double(:bike)
       broken_bike.report_broken
       subject.dock(broken_bike)
       expect {subject.release_bike}.to raise_error "This bike is broken"
     end
     it 'expects bike to be working' do
-      subject.dock(bike)
+      subject.dock(double(:bike))
       expect(subject.release_bike.working_status).to eq true
     end
   end
@@ -39,8 +39,8 @@ describe DockingStation do
   it { is_expected.to respond_to(:bikes)}
 
   it 'shows me that my bike is docked' do
-    subject.dock(bike)
-    expect(subject.bikes).to include(bike)
+    subject.dock(double(:bike))
+    expect(subject.bikes).to include(double(:bike))
   end
 
   it 'raises an error when #release_bike and the station is empty' do
@@ -48,7 +48,7 @@ describe DockingStation do
   end
 
   it 'raises an error when docking a bike when docking station is full' do
-    DockingStation::DEFAULT_CAPACITY.times{subject.dock(bike)}
-    expect {subject.dock(bike)}.to raise_error("This docking station is full")
+    DockingStation::DEFAULT_CAPACITY.times{subject.dock(double(:bike))}
+    expect {subject.dock(double(:bike))}.to raise_error("This docking station is full")
   end
 end
