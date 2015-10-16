b4f7ffd76e16001518d447ed23431c9ee43a11a4
dearshrewdwit <dearshrewdwit@gmail.com>
2015-10-16 14:28:39 +0100

    fixed iterating over single thing issue

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -8,7 +8,7 @@ describe Van do
   it { is_expected.to respond_to(:bikes) }
 
   it 'shows me that my bike is docked' do
-    subject.dock(bike)
+    subject.dock([bike])
     expect(subject.bikes).to include(bike)
   end
 
@@ -18,11 +18,10 @@ describe Van do
     bike1 = double(working_status: false)
     bike2 = double(working_status: false)
     bike3 = double(working_status: true)
-    array = [bike1, bike2]
-    array2 = [bike1, bike2, bike3]
-    subject.dock(bike1)
-    subject.dock(bike2)
-    subject.dock(bike3)
+    array = [bike1, bike2]   #  array2 = [bike1, bike2, bike3]
+    subject.dock([bike1])
+    subject.dock([bike2])
+    subject.dock([bike3])
     expect(subject.release_broken_bikes).to eq array
   end
 end
